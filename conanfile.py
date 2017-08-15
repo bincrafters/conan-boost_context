dbee344d3173a3d8338f22bc8e114cb44113ce2f
Jerry W <jerrywiltse@gmail.com>
2017-08-14 21:39:09 -0400

    mass update - minor recipe and cosmetic readme

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,8 @@ class BoostContextConan(ConanFile):
     version = "1.64.0"
     generators = "boost" 
     settings = "os", "arch", "compiler", "build_type"
-    url = "https://github.com/boostorg/context"
+    url = "https://github.com/bincrafters/conan-boost-context"
+    source_url = "https://github.com/boostorg/context"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "context"
@@ -21,7 +22,7 @@ class BoostContextConan(ConanFile):
                       
     def source(self):
         self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
-                 .format(self.version, self.url))
+                 .format(self.version, self.source_url))
 
     def build(self):
         boost_build = self.deps_cpp_info["Boost.Build"]
@@ -53,4 +54,4 @@ feature.compose <segmented-stacks>on : <define>BOOST_USE_SEGMENTED_STACKS ;
         self.copy(pattern="*", dst="lib", src=lib_dir)
 
     def package_info(self):
-        self.cpp_info.libs = ["boost_%s"%(self.lib_short_name)]
+        self.cpp_info.libs = self.collect_libs()

794ce6ea300a23e2ed1f084f595e232dd1ae4530
Jerry W <jerrywiltse@gmail.com>
2017-08-17 01:22:59 -0400

    moved toolset detection to generator

--- conanfile.py
+++ conanfile.py
@@ -30,16 +30,8 @@ class BoostContextConan(ConanFile):
         b2_bin_name = "b2.exe" if self.settings.os == "Windows" else "b2"
         b2_bin_dir_name = boost_build.bindirs[0]
         b2_full_path = os.path.join(boost_build.rootpath, b2_bin_dir_name, b2_bin_name)
-
-        toolsets = {
-          'gcc': 'gcc',
-          'Visual Studio': 'msvc',
-          'clang': 'clang',
-          'apple-clang': 'clang'}
-
-        b2_toolset = toolsets[str(self.settings.compiler)]
         
-        self.run(b2_full_path + " -j4 -a --hash=yes toolset=" + b2_toolset)
+        self.run(b2_full_path + " -j4 -a --hash=yes")
         
         with open(os.path.join(self.build_folder,"stage","lib","jamroot.jam"),"a") as f:
             f.write("""

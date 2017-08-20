c7b3da5787891c423c25f0cf261794a9c22bade4
Jerry W <jerrywiltse@gmail.com>
2017-08-20 00:45:14 -0400

    enabled short paths

--- conanfile.py
+++ conanfile.py
@@ -5,6 +5,7 @@ class BoostContextConan(ConanFile):
     version = "1.64.0"
     generators = "boost" 
     settings = "os", "arch", "compiler", "build_type"
+    short_paths = True
     url = "https://github.com/bincrafters/conan-boost-context"
     source_url = "https://github.com/boostorg/context"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
@@ -22,7 +23,7 @@ class BoostContextConan(ConanFile):
                       
     def source(self):
         for lib_short_name in self.lib_short_names:
-            self.run("git clone --depth=50 --branch=boost-{0} https://github.com/boostorg/{1}.git"
+            self.run("git clone --depth=1 --branch=boost-{0} https://github.com/boostorg/{1}.git"
                      .format(self.version, lib_short_name)) 
 
     def build(self):

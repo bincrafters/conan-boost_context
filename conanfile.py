6cd8d2c156168dd50c6504a3331689ecf407c6cf
Jerry W <jerrywiltse@gmail.com>
2017-09-19 23:30:11 -0400

    changed refs to testing

--- conanfile.py
+++ conanfile.py
@@ -13,13 +13,13 @@ class BoostContextConan(ConanFile):
     lib_short_names = ["context"]
     options = {"shared": [True, False]}
     default_options = "shared=False"
-    build_requires = "Boost.Generator/1.65.1@bincrafters/stable"
-    requires =  "Boost.Assert/1.65.1@bincrafters/stable", \
-                      "Boost.Config/1.65.1@bincrafters/stable", \
-                      "Boost.Pool/1.65.1@bincrafters/stable", \
-                      "Boost.Predef/1.65.1@bincrafters/stable", \
-                      "Boost.Smart_Ptr/1.65.1@bincrafters/stable", \
-                      "Boost.Level11Group/1.65.1@bincrafters/stable"
+    build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
+    requires =  "Boost.Assert/1.65.1@bincrafters/testing", \
+                      "Boost.Config/1.65.1@bincrafters/testing", \
+                      "Boost.Pool/1.65.1@bincrafters/testing", \
+                      "Boost.Predef/1.65.1@bincrafters/testing", \
+                      "Boost.Smart_Ptr/1.65.1@bincrafters/testing", \
+                      "Boost.Level11Group/1.65.1@bincrafters/testing"
 
                       #assert1 config0 pool11 predef0 smart_ptr4
                       

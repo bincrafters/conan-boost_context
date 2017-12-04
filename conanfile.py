51084a12cf1466973191b219f2df9cad86e29fd5
Rene Rivera <grafikrobot@gmail.com>
2017-12-03 23:37:26 -0600

    Next generation of Boost packages.

--- conanfile.py
+++ conanfile.py
@@ -1,63 +1,39 @@
 from conans import ConanFile, tools
-import os
 
 
 class BoostContextConan(ConanFile):
     name = "Boost.Context"
     version = "1.65.1"
-    generators = "boost" 
-    settings = "os", "arch", "compiler", "build_type"
-    short_paths = True
-    url = "https://github.com/bincrafters/conan-boost-context"
-    description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
-    license = "www.boost.org/users/license.html"
-    lib_short_names = ["context"]
     options = {"shared": [True, False]}
     default_options = "shared=False"
-    build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
-    requires =  "Boost.Assert/1.65.1@bincrafters/testing", \
-                      "Boost.Config/1.65.1@bincrafters/testing", \
-                      "Boost.Pool/1.65.1@bincrafters/testing", \
-                      "Boost.Predef/1.65.1@bincrafters/testing", \
-                      "Boost.Smart_Ptr/1.65.1@bincrafters/testing", \
-                      "Boost.Level11Group/1.65.1@bincrafters/testing"
+    requires = \
+        "Boost.Assert/1.65.1@bincrafters/testing", \
+        "Boost.Config/1.65.1@bincrafters/testing", \
+        "Boost.Pool/1.65.1@bincrafters/testing", \
+        "Boost.Predef/1.65.1@bincrafters/testing", \
+        "Boost.Smart_Ptr/1.65.1@bincrafters/testing", \
+        "Boost.Level11Group/1.65.1@bincrafters/testing"
+    lib_short_names = ["context"]
+    is_header_only = False
 
-                      #assert1 config0 pool11 predef0 smart_ptr4
-                      
-    def source(self):
-        boostorg_github = "https://github.com/boostorg"
-        archive_name = "boost-" + self.version  
-        for lib_short_name in self.lib_short_names:
-            tools.get("{0}/{1}/archive/{2}.tar.gz"
-                .format(boostorg_github, lib_short_name, archive_name))
-            os.rename(lib_short_name + "-" + archive_name, lib_short_name)
-
-    def build(self):
-        self.run(self.deps_user_info['Boost.Generator'].b2_command + " " + self.b2_args)
-        
-        with open(os.path.join(self.build_folder,"stage","lib","jamroot.jam"),"a") as f:
+    def build_after(self):
+        import os
+        with open(os.path.join(self.build_folder, "context", "lib", "jamroot.jam"), "a") as f:
             f.write("""
 import feature ;
 feature.feature segmented-stacks : on : optional propagated composite ;
 feature.compose <segmented-stacks>on : <define>BOOST_USE_SEGMENTED_STACKS ;
 """)
-        
-    def package(self):
-        self.copy(pattern="*", dst="lib", src="stage/lib")
-        for lib_short_name in self.lib_short_names:
-            include_dir = os.path.join(lib_short_name, "include")
-            self.copy(pattern="*", dst="include", src=include_dir)
 
-    def package_info(self):
-        self.user_info.lib_short_names = ",".join(self.lib_short_names)
-        self.cpp_info.libs = tools.collect_libs(self)
-        self.cpp_info.defines.append("BOOST_ALL_NO_LIB=1")
+    def b2_options(self, lib_name=None):
+        # pylint: disable=unused-argument
+        return " " + self.b2_args
 
     @property
     def b2_args(self):
-        binary_format_arg = "binary-format="+self.b2_binary_format if self.b2_binary_format else ""
-        abi_arg = "abi="+self.b2_abi if self.b2_abi else ""
-        return binary_format_arg+" "+abi_arg
+        binary_format_arg = "binary-format=" + self.b2_binary_format if self.b2_binary_format else ""
+        abi_arg = "abi=" + self.b2_abi if self.b2_abi else ""
+        return binary_format_arg + " " + abi_arg
 
     @property
     def b2_binary_format(self):
@@ -83,3 +59,30 @@ feature.compose <segmented-stacks>on : <define>BOOST_USE_SEGMENTED_STACKS ;
             return "aapcs"
         else:
             return None
+
+    # BEGIN
+
+    url = "https://github.com/bincrafters/conan-boost-context"
+    description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
+    license = "www.boost.org/users/license.html"
+    settings = "os", "arch", "compiler", "build_type"
+    short_paths = True
+    build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
+    generators = "boost"
+
+    def package_id(self):
+        if self.is_header_only:
+            self.info.header_only()
+
+    # pylint: disable=unused-import
+    @property
+    def env(self):
+        try:
+            with tools.pythonpath(super(self.__class__, self)):
+                import boostgenerator  # pylint: disable=F0401
+                boostgenerator.BoostConanFile(self)
+        except:
+            pass
+        return super(self.__class__, self).env
+
+    # END

20c07f4b90894cc6cff40701906f854e68b88b99
Jerry W <jerrywiltse@gmail.com>
2018-01-25 01:12:40 -0500

    Refresh round 2

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,8 @@ class BoostContextConan(ConanFile):
     name = "boost_context"
     version = "1.66.0"
     url = "https://github.com/bincrafters/conan-boost_context"
-    
+    author = "Bincrafters <bincrafters@gmail.com>"
+    exports = ["LICENSE.md"]
     lib_short_names = ["context"]
     is_header_only = False
     
